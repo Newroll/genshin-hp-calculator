a69b54522dc1df5d11f606f8e20316e12b7864b4
Newroll <robbinxxy@gmail.com>
2022-01-25 09:37:38 +1300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-//ur mom
 
 
 int main()
@@ -160,7 +159,7 @@ int main()
 			if (level == 18) {
 
 				levelmodifierone = 679.928;
-				levelmodifiertwo = 697.853
+				levelmodifiertwo = 697.853;
 				break;
 
 			}
@@ -213,6 +212,614 @@ int main()
 
 			}
 
+			if (level == 25) {
+
+				levelmodifierone = 1145.135;
+				levelmodifiertwo = 1191.353;
+				break;
+
+			}
+
+			if (level == 26) {
+
+				levelmodifierone = 1208.108;
+				levelmodifiertwo = 1255.221;
+				break;
+
+			}
+
+			if (level == 27) {
+
+				levelmodifierone = 1272.347;
+				levelmodifiertwo = 1322.928;
+				break;
+
+			}
+
+			if (level == 28) {
+
+				levelmodifierone = 1337.866;
+				levelmodifiertwo = 1393.131;
+				break;
+
+			}
+
+			if (level == 29) {
+
+				levelmodifierone = 1404.679;
+				levelmodifiertwo = 1488.315;
+				break;
+
+			}
+
+			if (level == 30) {
+
+				levelmodifierone = 1472.800;
+				levelmodifiertwo = 1584.970;
+				break;
+
+			}
+
+			if (level == 31) {
+
+				levelmodifierone = 1554.986;
+				levelmodifiertwo = 1683.133;
+				break;
+
+			}
+
+			if (level == 32) {
+
+				levelmodifierone = 1638.968;
+				levelmodifiertwo = 1784.843;
+				break;
+
+			}
+
+			if (level == 33) {
+
+				levelmodifierone = 1708.106;
+				levelmodifiertwo = 1890.934;
+				break;
+
+			}
+			
+			if (level == 34) {
+
+				levelmodifierone = 2020.002;
+				levelmodifiertwo = 2001.498;
+				break;
+
+			}
+
+			if (level == 35) {
+
+				levelmodifierone = 2088.602;
+				levelmodifiertwo = 2127.689;
+				break;
+
+			}
+
+			if (level == 36) {
+
+				levelmodifierone = 2158.307;
+				levelmodifiertwo = 2260.648;
+				break;
+
+			}
+
+			if (level == 37) {
+
+				levelmodifierone = 2253.314;
+				levelmodifiertwo = 2398.972;
+				break;
+
+			}
+
+			if (level == 38) {
+
+				levelmodifierone = 2357.923;
+				levelmodifiertwo = 2541.917;
+				break;
+
+			}
+
+			if (level == 39) {
+
+				levelmodifierone = 2464.276;
+				levelmodifiertwo = 2691.184;
+				break;
+
+			}
+
+			if (level == 40) {
+
+				levelmodifierone = 2694.849;
+				levelmodifiertwo = 2845.847;
+				break;
+
+			}
+
+			if (level == 41) {
+
+				levelmodifierone = 2825.174;
+				levelmodifiertwo = 3006.931;
+				break;
+
+			}
+
+			if (level == 42) {
+
+				levelmodifierone = 2957.735;
+				levelmodifiertwo = 3182.917;
+				break;
+
+			}
+
+			if (level == 43) {
+
+				levelmodifierone = 3083.906;
+				levelmodifiertwo = 3371.801;
+				break;
+
+			}
+
+			if (level == 44) {
+
+				levelmodifierone = 3212.145;
+				levelmodifiertwo = 3573.107;
+				break;
+
+			}
+
+			if (level == 45) {
+
+				levelmodifierone = 3593.158;
+				levelmodifiertwo = 3823.187;
+				break;
+
+			}
+
+			if (level == 46) {
+
+				levelmodifierone = 3718.716;
+				levelmodifiertwo = 4077.436;
+				break;
+
+			}
+
+			if (level == 47) {
+
+				levelmodifierone = 3934.571;
+				levelmodifiertwo = 4346.025;
+				break;
+
+			}
+
+			if (level == 48) {
+
+				levelmodifierone = 4154.840;
+				levelmodifiertwo = 4629.380;
+				break;
+
+			}
+
+			if (level == 49) {
+
+				levelmodifierone = 4379.583;
+				levelmodifiertwo = 4928.888;
+				break;
+
+			}
+
+			if (level == 50) {
+
+				levelmodifierone = 4996.484;
+				levelmodifiertwo = 5243.532;
+				break;
+
+			}
+
+			if (level == 51) {
+
+				levelmodifierone = 5224.296;
+				levelmodifiertwo = 5512.657;
+				break;
+
+			}
+
+			if (level == 52) {
+
+				levelmodifierone = 5454.720;
+				levelmodifiertwo = 5796.984;
+				break;
+
+			}
+
+			if (level == 53) {
+
+				levelmodifierone = 5687.789;
+				levelmodifiertwo = 6090.197;
+				break;
+
+			}
+
+			if (level == 54) {
+
+				levelmodifierone = 5923.530;
+				levelmodifiertwo = 6392.746;
+				break;
+
+			}
+
+			if (level == 55) {
+
+				levelmodifierone = 6146.911;
+				levelmodifiertwo = 6803.885;
+				break;
+
+			}
+
+			if (level == 56) {
+
+				levelmodifierone = 6333.418;
+				levelmodifiertwo = 7187.701;
+				break;
+
+			}
+
+			if (level == 57) {
+
+				levelmodifierone = 6572.412;
+				levelmodifiertwo = 7582.394;
+				break;
+
+			}
+
+			if (level == 58) {
+
+				levelmodifierone = 6835.255;
+				levelmodifiertwo = 7988.528;
+				break;
+
+			}
+
+			if (level == 59) {
+
+				levelmodifierone = 7101.442;
+				levelmodifiertwo = 8416.958;
+				break;
+
+			}
+
+			if (level == 60) {
+
+				levelmodifierone = 8381.255;
+				levelmodifiertwo = 8858.942;
+				break;
+
+			}
+
+			if (level == 61) {
+
+				levelmodifierone = 8657.451;
+				levelmodifiertwo = 9310.857;
+				break;
+
+			}
+
+			if (level == 62) {
+
+				levelmodifierone = 8961.233;
+				levelmodifiertwo = 9852.285;
+				break;
+
+			}
+
+			if (level == 63) {
+
+				levelmodifierone = 9268.136;
+				levelmodifiertwo = 10411.526;
+				break;
+
+			}
+
+			if (level == 64) {
+
+				levelmodifierone = 9668.122;
+				levelmodifiertwo = 10983.546;
+				break;
+
+			}
+
+			if (level == 65) {
+
+				levelmodifierone = 9982.892;
+				levelmodifiertwo = 11646.845;
+				break;
+
+			}
+
+			if (level == 66) {
+
+				levelmodifierone = 10241.750;
+				levelmodifiertwo = 12221.555;
+				break;
+
+			}
+
+			if (level == 67) {
+
+				levelmodifierone = 11265.319;
+				levelmodifiertwo = 12815.883;
+				break;
+
+			}
+
+			if (level == 68) {
+
+				levelmodifierone = 11619.707;
+				levelmodifiertwo = 13414.004;
+				break;
+
+			}
+
+			if (level == 69) {
+
+				levelmodifierone = 11949.945;
+				levelmodifiertwo = 14030.204;
+				break;
+
+			}
+
+			if (level == 70) {
+
+				levelmodifierone = 13051.973;
+				levelmodifiertwo = 14652.096;
+				break;
+
+			}
+
+			if (level == 71) {
+
+				levelmodifierone = 13410.889;
+				levelmodifiertwo = 15283.167;
+				break;
+
+			}
+
+			if (level == 72) {
+
+				levelmodifierone = 13805.527;
+				levelmodifiertwo = 15983.958;
+				break;
+
+			}
+
+			if (level == 73) {
+
+				levelmodifierone = 14092.246;
+				levelmodifiertwo = 16647.155;
+				break;
+
+			}
+
+			if (level == 74) {
+
+				levelmodifierone = 14504.316;
+				levelmodifiertwo = 17341.355;
+				break;
+
+			}
+
+			if (level == 75) {
+
+				levelmodifierone = 14920.828;
+				levelmodifiertwo = 18333.969;
+				break;
+
+			}
+
+			if (level == 76) {
+
+				levelmodifierone = 15265.360;
+				levelmodifiertwo = 19284.010;
+				break;
+
+			}
+
+			if (level == 77) {
+
+				levelmodifierone = 15663.968;
+				levelmodifiertwo = 20273.352;
+				break;
+
+			}
+
+			if (level == 78) {
+
+				levelmodifierone = 16066.612;
+				levelmodifiertwo = 21278.437;
+				break;
+
+			}
+
+			if (level == 79) {
+
+				levelmodifierone = 16439.805;
+				levelmodifiertwo = 22358.461;
+				break;
+
+			}
+
+			if (level == 80) {
+
+				levelmodifierone = 18565.728;
+				levelmodifiertwo = 23636.617;
+				break;
+
+			}
+
+			if (level == 81) {
+
+				levelmodifierone = 18947.872;
+				levelmodifiertwo = 24891.897;
+				break;
+
+			}
+
+			if (level == 82) {
+
+				levelmodifierone = 19333.386;
+				levelmodifiertwo = 26169.663;
+				break;
+
+			}
+
+			if (level == 83) {
+
+				levelmodifierone = 19573.308;
+				levelmodifiertwo = 27414.775;
+				break;
+
+			}
+
+			if (level == 84) {
+
+				levelmodifierone = 20178.597;
+				levelmodifiertwo = 28658.140;
+				break;
+
+			}
+
+			if (level == 85) {
+
+				levelmodifierone = 20424.359;
+				levelmodifiertwo = 30198.153;
+				break;
+
+			}
+
+			if (level == 86) {
+
+				levelmodifierone = 20823.049;
+				levelmodifiertwo = 31476.422;
+				break;
+
+			}
+
+			if (level == 87) {
+
+				levelmodifierone = 21244.643;
+				levelmodifiertwo = 32775.531;
+				break;
+
+			}
+
+			if (level == 88) {
+
+				levelmodifierone = 21718.220;
+				levelmodifiertwo = 34096.897;
+				break;
+
+			}
+
+			if (level == 89) {
+
+				levelmodifierone = 22197.342;
+				levelmodifiertwo = 35506.446;
+				break;
+
+			}
+
+			if (level == 90) {
+
+				levelmodifierone = 24354.087;
+				levelmodifiertwo = 37100.287;
+				break;
+
+			}
+
+			if (level == 91) {
+
+				levelmodifierone = 24932.385;
+				levelmodifiertwo = 38469.363;
+				break;
+
+			}
+
+			if (level == 92) {
+
+				levelmodifierone = 25570.901;
+				levelmodifiertwo = 39857.282;
+				break;
+
+			}
+
+			if (level == 93) {
+
+				levelmodifierone = 26217.758;
+				levelmodifiertwo = 41617.236;
+				break;
+
+			}
+
+			if (level == 94) {
+
+				levelmodifierone = 27122.642;
+				levelmodifiertwo = 43354.761;
+				break;
+
+			}
+
+			if (level == 95) {
+
+				levelmodifierone = 27748.838;
+				levelmodifiertwo = 45428.659;
+				break;
+
+			}
+
+			if (level == 96) {
+
+				levelmodifierone = 27923.105;
+				levelmodifiertwo = 47481.524;
+				break;
+
+			}
+
+			if (level == 97) {
+
+				levelmodifierone = 28116.286;
+				levelmodifiertwo = 49509.205;
+				break;
+
+			}
+
+			if (level == 98) {
+
+				levelmodifierone = 30658.232;
+				levelmodifiertwo = 51500.591;
+				break;
+
+			}
+
+			if (level == 99) {
+
+				levelmodifierone = 30869.964;
+				levelmodifiertwo = 53452.946;
+				break;
+
+			}
+
+			if (level == 100) {
+
+				levelmodifierone = 36765.122;
+				levelmodifiertwo = 55460.018;
+				break;
+
+			}
+
 			else {
 
 				std::cout << "Please specify a valid level! (1 - 100)\n";
@@ -223,12 +830,524 @@ int main()
 
 		//asks for enemy
 		while (true) {
+			std::cout << "\n";
 			std::cout << "What's the enemy?\n";
 			std::cin >> enemyname;
+
+			if (enemyname == "cicins") {
+
+				hp = (0.5 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "slimes") {
+
+				hp = (0.8 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "largeslimes") {
+
+				hp = (2 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "whopperflowers") {
+
+				hp = (3 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "hilichurls") {
+
+				hp = (1 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "hilichurlguards") {
+
+				hp = (1 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "hilichurlgrenadiers") {
+
+				hp = (1 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "hilichurlgrenadierscryo") {
+
+				hp = (0.8 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "hilichurlshooters") {
+
+				hp = (0.8 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "samachurls") {
+
+				hp = (1 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "unusualhilichurls") {
+
+				hp = (1.6 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "anemoboxers") {
+
+				hp = (3 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "electrohammers") {
+
+				hp = (4 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "geochanters") {
+
+				hp = (3 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "pyroslingers") {
+
+				hp = (3 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "cryogunners") {
+
+				hp = (4 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "hydrogunners") {
+
+				hp = (3 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "potioneers") {
+
+				hp = (1.6 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "scouts") {
+
+				hp = (1.2 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "marksmen") {
+
+				hp = (1.2 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "crushers") {
+
+				hp = (2 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "gravediggers") {
+
+				hp = (2 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "handymans") {
+
+				hp = (2 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "pugilists") {
+
+				hp = (2 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "nobushis") {
+
+				hp = (2 * levelmodifiertwo);
+				break;
+
+			}
+
+			if (enemyname == "kairagis") {
+
+				hp = (4.8 * levelmodifiertwo);
+				break;
+
+			}
+
+			if (enemyname == "mitachurls") {
+
+				hp = (3 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "lawachurls") {
+
+				hp = (9 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "abyssmages") {
+
+				hp = (2 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "abyssheralds") {
+
+				hp = (8 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "abysslectors") {
+
+				hp = (8 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "ruinguards") {
+
+				hp = (7 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "ruinhunters") {
+
+				hp = (6 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "ruingraders") {
+
+				hp = (9 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "ruincruisers") {
+
+				hp = (2 * levelmodifiertwo);
+				break;
+
+			}
+
+			if (enemyname == "ruindestroyers") {
+
+				hp = (3 * levelmodifiertwo);
+				break;
+
+			}
+
+			if (enemyname == "ruindefenders") {
+
+				hp = (4.2 * levelmodifiertwo);
+				break;
+
+			}
+
+			if (enemyname == "ruinscouts") {
+
+				hp = (4.2 * levelmodifiertwo);
+				break;
+
+			}
+
+			if (enemyname == "cicinmages") {
+
+				hp = (5 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "agents") {
+
+				hp = (5 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "mirrormaiden") {
+
+				hp = (6 * levelmodifiertwo);
+				break;
+
+			}
+
+			if (enemyname == "eyeofthestrom") {
+
+				hp = (5 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "geovishap") {
+
+				hp = (8 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "geovishaphatching") {
+
+				hp = (3 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "hypostases") {
+
+				hp = (7 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "eventhypostates") {
+
+				hp = (11 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "regisvines") {
+
+				hp = (7 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "eventregisvines") {
+
+				hp = (11 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "oceanids") {
+
+				hp = (10 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "cranes") {
+
+				hp = (0.8 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "raptors") {
+
+				hp = (0.8 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "mallards") {
+
+				hp = (1.6 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "crabs") {
+
+				hp = (1.6 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "boars") {
+
+				hp = (1.6 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "ferrets") {
+
+				hp = (1.4 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "frogs") {
+
+				hp = (3 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "finches") {
+
+				hp = (3 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "eventoceanids") {
+
+				hp = (14 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "eventcrabs") {
+
+				hp = (0.6 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "eventboars") {
+
+				hp = (3.4 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "eventferrets") {
+
+				hp = (3.4 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "primogeovishaps") {
+
+				hp = (30 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "maguukenki") {
+
+				hp = (20 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "perpectualmechanicalarray") {
+
+				hp = (16 * levelmodifiertwo);
+				break;
+
+			}
+
+			if (enemyname == "andrius") {
+
+				hp = (18 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "dvalin") {
+
+				hp = (9 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "onechilde") {
+
+				hp = (6 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "twochilde") {
+
+				hp = (6 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "threechilde") {
+
+				hp = (10 * levelmodifierone);
+				break;
+
+			}
 
 			if (enemyname == "azhdaha") {
 
 				hp = (36 * levelmodifierone);
+				break;
+
+			}
+
+			if (enemyname == "rockfondrifthoundwhelp") {
+
+				hp = (2.5 * levelmodifiertwo);
+				break;
+
+			}
+
+			if (enemyname == "thundercravenrifthoundwhelp") {
+
+				hp = (2.5 * levelmodifiertwo);
+				break;
+
+			}
+
+			if (enemyname == "rockfondrifthound") {
+
+				hp = (7 * levelmodifiertwo);
+				break;
+
+			}
+
+			if (enemyname == "thundercravenrifthound") {
+
+				hp = (7 * levelmodifiertwo);
 				break;
 
 			}
@@ -240,11 +1359,13 @@ int main()
 			}
 		}
 
+		std::cout << "\n";
 		std::cout << "The HP of " << enemyname << " at level " << level << " is " << hp;
 		std::cout << "\n";
 
 
 		//multiplier parts
+		std::cout << "\n";
 		std::cout << "Is there an HP multiplier? (https://genshin-impact.fandom.com/wiki/Level_Scaling/Enemy#Special_HP_Multiplier)\nIf so state it.\n";
 		std::cin >> multiplier;
 		std::cout << (multiplier * hp);
